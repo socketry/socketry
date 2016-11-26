socketry/socketry

commit 124a50a06d691fa1db7cbce374d1335434adf5b5

Socketry::SSL::Socket#from_socket specs
Also a small API change

lib/socketry/ssl/socket.rb
@@ -61,6 +61,7 @@ module Socketry
 
         @ssl_socket = OpenSSL::SSL::SSLSocket.new(@socket, @ssl_context)
         @ssl_socket.hostname = remote_addr if enable_sni
+        @ssl_socket.sync_close = true
 
         begin
           @ssl_socket.connect_nonblock
@@ -91,15 +92,14 @@ module Socketry
 
       # Wrap a Ruby OpenSSL::SSL::SSLSocket (or other low-level SSL socket)
       #
-      # @param socket [::Socket] (or specified socket_class) low-level socket to wrap
       # @param ssl_socket [OpenSSL::SSL::SSLSocket] SSL socket class associated with this socket
+      #
       # @return [self]
-      def from_socket(socket, ssl_socket)
-        raise TypeError, "expected #{@socket_class}, got #{socket.class}" unless socket.is_a?(@socket_class)
+      def from_socket(ssl_socket)
         raise TypeError, "expected #{@ssl_socket_class}, got #{ssl_socket.class}" unless ssl_socket.is_a?(@ssl_socket_class)
-        raise StateError, "already connected" if @socket && @socket != socket
+        raise StateError, "already connected" if @socket
 
-        @socket = socket
+        @socket = ssl_socket.to_io
         @ssl_socket = ssl_socket
         @ssl_socket.sync_close = true
 
@@ -110,6 +110,7 @@ module Socketry
       #
       # @param size [Fixnum] number of bytes to attempt to read
       # @param outbuf [String, NilClass] an optional buffer into which data should be read
+      #
       # @raise [Socketry::Error] an I/O operation failed
       # @return [String, :wait_readable] data read, or :wait_readable if operation would block
       def read_nonblock(size, outbuf: nil)
@@ -125,6 +126,7 @@ module Socketry
       # Perform a non-blocking write operation
       #
       # @param data [String] number of bytes to attempt to read
+      #
       # @raise [Socketry::Error] an I/O operation failed
       # @return [Fixnum, :wait_writable] number of bytes written, or :wait_writable if op would block
       def write_nonblock(data)

spec/socketry/ssl/socket_spec.rb
@@ -78,7 +78,26 @@ RSpec.describe Socketry::SSL::Socket do
   end
 
   describe "#from_socket" do
-    it "needs tests!"
+    let(:server_cert_file) { ssl_cert_path("trusted-cert") }
+    let(:server_key_file)  { ssl_key_path("trusted-cert") }
+
+    before { ssl_server_thread }
+
+    after do
+      ssl_server_thread.kill if ssl_server_thread.alive?
+      ssl_server.close rescue nil
+    end
+
+    it "creates SSL sockets from TCP sockets" do
+      ssl_context = OpenSSL::SSL::SSLContext.new
+      ssl_context.set_params(ssl_client_params)
+      tcp_socket = ::TCPSocket.new(remote_host, remote_port)
+      ssl_socket = OpenSSL::SSL::SSLSocket.new(tcp_socket, ssl_context)
+      ssl_socket.connect
+
+      expect(described_class.new.from_socket(ssl_socket)).to be_a described_class
+      ssl_socket.close
+    end
   end
 
   context "stream socket specs" do

commit 2a5a6ab2292a22815a533fd90069639530f1a774

Fix Socketry::SSL::Socket#close
124a50a0 introduced a regression in the close behavior on Ruby 2.2.x
by adding the following line:

    @ssl_socket.sync_close = true

This closes the underlying TCP socket when the OpenSSL::SSL::SSLSocket wrapping
it is closed.

Unfortunately Socketry::SSL::Socket#close was calling "super" which invoked the
corresponding ::TCPSocket close methods, but at the *end* after closing the
OpenSSL::SSL::SSLSocket, which already took care of closing the ::TCPSocket
thanks to `#sync_close = true`

We still want to call super though, to ensure the instance variables associated
with the TCP socket are properly cleared.

Solution: call `super` *first* to close the TCP socket and clear the associated
instance variables, then close the OpenSSL::SSL::SSLSocket. This ensures both
are closed, their associated instance variables cleared, and won't error out if
the TCP socket is already closed.

This should ensure that all sockets are closed in all cases, even if someone
manages to obtain the raw OpenSSL::SSL::SSLSocket or its underlying ::TCPSocket.

lib/socketry/ssl/socket.rb
@@ -163,8 +163,10 @@ module Socketry
       #
       # @return [true, false] true if the socket was open, false if closed
       def close
-        @ssl_socket.close rescue nil
-        super
+        return false unless super
+        @ssl_socket.close
+        @ssl_socket = nil
+        true
       end
 
       private