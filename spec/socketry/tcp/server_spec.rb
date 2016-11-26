547017d8c643727c6146c47447974e4dc46900fa
Tony Arcieri <bascule@gmail.com>
2016-11-26 00:16:26 -0800

    Socketry::TCP::Server.open specs

--- spec/socketry/tcp/server_spec.rb
+++ spec/socketry/tcp/server_spec.rb
@@ -6,17 +6,33 @@ RSpec.describe Socketry::TCP::Server do
 
   subject(:tcp_server) { described_class.new(bind_addr, bind_port) }
 
-  before { tcp_server }
-  after  { tcp_server.close rescue nil }
-
   describe ".new" do
+    before { tcp_server }
+    after  { tcp_server.close rescue nil }
+
     it "raises Socketry::AddressInUseError if an address is in use" do
       # Attempt to bind to the same host/port again
       expect { described_class.new(bind_addr, bind_port) }.to raise_error(Socketry::AddressInUseError)
     end
   end
 
+  describe ".open" do
+    it "yields an instance of its class" do
+      block_called = false
+
+      described_class.open(bind_addr, bind_port) do |server|
+        expect(server).to be_a described_class
+        block_called = true
+      end
+
+      expect(block_called).to eq true
+    end
+  end
+
   describe "#accept" do
+    before { tcp_server }
+    after  { tcp_server.close rescue nil }
+
     it "accepts connections" do
       begin
         client = TCPSocket.new(bind_addr, bind_port)
